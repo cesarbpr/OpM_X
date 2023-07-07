dab5a1d4d1041eb98b01ccec98c7b69cf04ed6cc
cesarbpr <cesar.bracamonte@utec.edu.pe>
2023-07-07 15:06:10 -0500

    se grafica uuna gráfica sin error

--- simulacion_ac/src/graficador.py
+++ simulacion_ac/src/graficador.py
@@ -1,42 +1,52 @@
 #!/usr/bin/env python3
 import rospy
+import numpy as np
 from std_msgs.msg import Float64MultiArray
 import matplotlib.pyplot as plt
 
 # Variables globales para almacenar los datos recibidos
+global x_data, y_data, refresh_point, q_vec
 x_data = []
 y_data = []
-q_temp = ([0,0])
-def cb_function(msg):
-    global x_data, y_data, q_temp
-    current_time = rospy.Time.now().to_sec()  # Obtener el tiempo actual en segundos
-    x_data.append(current_time)  # Tiempo actual como eje x
-    q_temp = msg.data
-    print(q_temp[1])
-    y_data.append(q_temp[1]) 
+q_vec = [0,0]
+refresh_point = 500    # Punto en el cual se borra la data pasada y se actualiza
+def cb_function(q_des):
+    global q_vec
+    q_vec=q_des.data
     #rospy.loginfo(msg)
 
+def create_point():
+    global x_data, y_data, refresh_point, q_vec
+    current_time = rospy.Time.now().to_sec()  # Obtener el tiempo actual en segundos
+    q_des1=q_vec[0]
+    q_des2=q_vec[1]
+    x_data.append(current_time)  # Tiempo actual como eje x
+    y_data.append(q_des1)
+        # Actualizar y borrar la data pasada cuando se alcanza el refresh_point
+    if len(x_data) >= refresh_point:
+        x_data = x_data[-refresh_point:]
+        y_data = y_data[-refresh_point:]
 if __name__ == "__main__":
 
     rospy.init_node('q_graph')
-    rospy.Subscriber('q_ref',Float64MultiArray,cb_function)
+    rospy.Subscriber('q_des',Float64MultiArray,cb_function)
 
     plt.ion()  # Modo interactivo de matplotlib
     fig, ax = plt.subplots()  # Crear una figura y un conjunto de ejes
     line, = ax.plot(x_data, y_data)  # Crear una línea para graficar los datos
-
-
 
     plt.xlabel('Tiempo')
     plt.ylabel('Valor')
     plt.title('Grafica en tiempo real')
 
     while not rospy.is_shutdown():
-        line.set_xdata(x_data)  # Actualizar los datos del eje x
-        line.set_ydata(y_data)  # Actualizar los datos del eje y
-        ax.relim()  # Actualizar los límites de los ejes
-        ax.autoscale_view()  # Ajustar la escala de los ejes automáticamente
-        plt.pause(0.01)  # Pausar para permitir la actualización del gráfico
+        create_point()
+        if len(x_data) == len(y_data):
+            line.set_xdata(x_data)  # Actualizar los datos del eje x
+            line.set_ydata(y_data)  # Actualizar los datos del eje y
+            ax.relim()  # Actualizar los límites de los ejes
+            ax.autoscale_view()  # Ajustar la escala de los ejes automáticamente
+            plt.pause(0.01)  # Pausar para permitir la actualización del gráfico
 
 
     plt.ioff()  # Desactivar el modo interactivo de matplotlib
